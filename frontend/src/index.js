fe491268bbd08394753c7ade1d947c5f20ac9e3b
Andrej <70228739+AndrejTS@users.noreply.github.com>
2022-11-11 23:34:55 +0100

    add base filtering on frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,20 +7,61 @@ function App() {
 
 function Tours() {
   const [tours, setTours] = useState([]);
+  const [filtering, setFiltering] = useState({
+    minPrice: '',
+    maxPrice: '',
+  });
 
   const fetchData = async () => {
+    // fetch("/tours", {
+    //   method: 'GET',
+    //   body: JSON.stringify(filtering),
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    // })
+
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
     return setTours(data);
   };
 
-  //
   React.useEffect(() => {
     fetchData();
-  }, []);
+  });
+
+  function changeFiltering(event) {
+    const value = event.target.value;
+    setFiltering({
+      ...filtering,
+      [event.target.name]: value,
+    });
+  }
 
   return (
     <div class="tours">
+      <form onSubmit={''}>
+        <label>
+          Min Price:
+          <input
+            name="minPrice"
+            type="text"
+            value={filtering.minPrice}
+            onChange={changeFiltering}
+          />
+        </label>
+        <label>
+          Max Price:
+          <input
+            name="maxPrice"
+            type="text"
+            value={filtering.maxPrice}
+            onChange={changeFiltering}
+          />
+        </label>
+        <input type="submit" value="Filter" />
+      </form>
+
       <h1>Tours</h1>
       {tours.map((tour) => (
         <Tour obj={tour} />
@@ -34,7 +75,7 @@ function Tour({ obj }) {
     <div class="tour">
       <p>{obj.name}</p>
       <p>{obj.address.city}</p>
-      <br/>
+      <br />
     </div>
   );
 }
