c47945f2fd9fd80d6abb50e7ac3e43a10905a85e
Andrej <70228739+AndrejTS@users.noreply.github.com>
2022-11-13 19:41:55 +0100

    - use react-bootstrap - add provisory edit modal

--- frontend/src/components/Tour.js
+++ frontend/src/components/Tour.js
@@ -1,24 +1,52 @@
 import { useState } from 'react';
-import Modal from './DeleteModal';
+import { Modal, Button } from 'react-bootstrap';
 import { FaTimes } from 'react-icons/fa';
 
 function Tour({ obj, deleteTour }) {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
+  const [editModalIsOpen, setEditModalIsOpen] = useState(false);
 
   return (
     <div className="tour">
       <p>{obj.name}</p>
       <FaTimes
         style={{ color: 'red', cursor: 'pointer' }}
-        onClick={() => setModalIsOpen(true)}
+        onClick={() => setDeleteModalIsOpen(true)}
       />
+
       <Modal
-        tourId={obj.id}
-        deleteTour={deleteTour}
-        handleClose={() => setModalIsOpen(false)}
-        modalIsOpen={modalIsOpen}
-      />
+        show={deleteModalIsOpen}
+        onHide={() => setDeleteModalIsOpen(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Tour</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Delete tour {obj.id}?</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => deleteTour(obj.id)}>Delete</Button>
+          <Button
+            variant="secondary"
+            onClick={() => setDeleteModalIsOpen(false)}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <p>{obj.address.city}</p>
+
+      <Modal show={editModalIsOpen} onHide={() => setEditModalIsOpen(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Tour</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{obj}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setEditModalIsOpen(false)}>
+            Close Button
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <br />
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Tours from './components/Tours';
 
